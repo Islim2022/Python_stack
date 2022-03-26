271d7a965d3090fc17e318f10dbcf3805c553130
Hanei Islim <100063345+Islim2022@users.noreply.github.com>
2022-03-26 13:17:45 +0300

    Add files via upload

---
+++ _python/flask/flask_fundamentals/Dojo Survey/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/results', methods=['POST'])
+def create_dojo():
+    print(request.form)
+    name = request.form['name']
+    locations = request.form['locations']
+    language = request.form['languages']
+    comment = request.form['comment']
+    return render_template("result.html", name = name, location = locations, language = language, comment = comment)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
